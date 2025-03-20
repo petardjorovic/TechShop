e013797735ca6f7c9edb5529266b977d3cc7e769
petardjorovic <perodjorovic@gmail.com>
2025-03-20 21:55:23 +0100

    correct server

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //* routes
-app.use(require('./routes'));
+app.use('/', require('./routes'));
 
 //* Error 404
 app.all('*', (req, res, next) => {
