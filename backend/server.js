9c339f6dc2d3d7201851b27fdc937b19f88642c4
petardjorovic <perodjorovic@gmail.com>
2025-03-27 14:32:26 +0100

    Configure server.js and correction

--- backend/server.js
+++ backend/server.js
@@ -22,18 +22,9 @@ app.use(cors());
 
 //* Ucitaj mi staticke fajlove
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'dist')));
-}
 
 //* routes
 app.use('/', require('./routes'));
-
-if (process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-    });
-}
 
 //* Error 404
 app.all('*', (req, res, next) => {
